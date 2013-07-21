e9bd3246c1a6b3ec9efc56b1f819b582474930fc
Abu Sultanov <neurofag@gmail.com>
2013-07-22 02:23:13 +0400

    all correct solutions

--- python_lab.py
+++ python_lab.py
@@ -30,7 +30,7 @@ Y2 = { 3,6,12 }
 ## Task 9
 base = 10
 digits = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-three_digits_set = three_digit = {a*base**2 + b*base**1+c*base**0 for a in digits for b in digits for c in digits }
+three_digits_set =  {a*base**2 + b*base**1+c*base**0 for a in digits for b in digits for c in digits }
 
 ## Task 10
 S = {1, 2, 3, 4}
@@ -38,77 +38,77 @@ T = {3, 4, 5, 6}
 S_intersect_T = { c for c in S if c in T }
 
 ## Task 11
-L_average = ... # average of: [20, 10, 15, 75]
+L_average = sum([20,10,15,75])/len([20,10,15,75]) # average of: [20, 10, 15, 75]
 
 ## Task 12
 LofL = [[.25, .75, .1], [-1, 0], [4, 4, 4, 4]]
-LofL_sum = ... # use form: sum([sum(...) ... ])
+LofL_sum = sum([sum(_) for _ in LofL]) # use form: sum([sum(...) ... ])
 
 ## Task 13
-cartesian_product = ... # use form: [ ... {'A','B','C'} ... {1,2,3} ... ]
+cartesian_product =  [ [l,n] for l in  {'A','B','C'} for n in  {1,2,3} ]
 
 ## Task 14
 S = {-4, -2, 1, 2, 5, 0}
-zero_sum_list = [ ... ] 
+zero_sum_list = [ (a,b,c) for a in S for b in S for c in S if not sum((a,b,c))]
 
 ## Task 15
-exclude_zero_list = [ ... ]
+exclude_zero_list =  [ (a,b,c) for a in S for b in S for c in S if not sum((a,b,c)) and  (a or  b or  c)]
 
 ## Task 16
-first_of_tuples_list = ...
+first_of_tuples_list =  [ (a,b,c) for a in S for b in S for c in S if not sum((a,b,c)) and  (a or  b or  c)][0]
 
 ## Task 17
-L1 = [...] # <-- want len(L1) != len(list(set(L1)))
-L2 = [...] # <-- same len(L2) == len(list(set(L2))) but L2 != list(set(L2))
+L1 = [1,2,3,3] # <-- want len(L1) != len(list(set(L1)))
+L2 = [-1,-2,0, 3, 5] # <-- same len(L2) == len(list(set(L2))) but L2 != list(set(L2))
 
 ## Task 18
-odd_num_list_range = {...}
+odd_num_list_range = { c for c in range(1,100,2) }
 
 ## Task 19
 L = ['A','B','C','D','E']
-range_and_zip = ...
+range_and_zip = list(zip(range(5), L))
 
 ## Task 20
-list_sum_zip = [...]
+list_sum_zip = [sum(x) for x in zip([10,25,40],[1,15,20])]
 
 ## Task 21
 dlist = [{'James':'Sean', 'director':'Terence'}, {'James':'Roger', 'director':'Lewis'}, {'James':'Pierce', 'director':'Roger'}]
 k = 'James'
-value_list = [...]
+value_list = [d[k] for d  in dlist]
 
 ## Task 22
 dlist = [{'Bilbo':'Ian','Frodo':'Elijah'},{'Bilbo':'Martin','Thorin':'Richard'}]
 k = 'Bilbo'
-value_list_modified_1 = [...] # <-- Use the same expression here
+value_list_modified_1 = [d[k]  if k in d else "NOT PRESENT" for d in dlist] # <-- Use the same expression here
 k = 'Frodo'
-value_list_modified_2 = [...] # <-- as you do here
+value_list_modified_2 = [d[k]  if k in d else "NOT PRESENT" for d in dlist] # <-- as you do here
 
 ## Task 23
-square_dict = {...}
+square_dict = {x: x*x for x in range(0,100)}
 
 ## Task 24
 D = {'red','white','blue'}
-identity_dict = {...}
+identity_dict = {c:c for c in list(D)}
 
 ## Task 25
 base = 10
 digits = set(range(10))
-representation_dict = {...}
+representation_dict = {k:v for k,v in zip(range(0,base**3),[[a,b,c] for a in digits for b in digits for c in digits])}
 
 ## Task 26
 d = {0:1000.0, 1:1200.50, 2:990}
 names = ['Larry', 'Curly', 'Moe']
-listdict2dict = { ... }
+listdict2dict = { names[i]:d[i]  for i in  range(len(names)) }
 
 ## Task 27
-def nextInts(L): return [ ... ]
+def nextInts(L): return [ x+1 for x in L]
 
 ## Task 28
-def cubes(L): return [ ... ] 
+def cubes(L): return [ x*x*x for x in L ] 
 
 ## Task 29
-def dict2list(dct, keylist): return [ ... ]
+def dict2list(dct, keylist): return [ dct[keylist[i]] for i in range(len(keylist)) ]
 
 ## Task 30 
-def list2dict(L, keylist): return { ... } 
+def list2dict(L, keylist): return { keylist[i]:L[i] for i in range(len(L)) } 
 
