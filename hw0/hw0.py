7d71ddc45d596562aa0f0848dd7813bc9d10f08e
Abu Sultanov <neurofag@gmail.com>
2013-07-29 14:36:42 +0400

    solutions file

---
+++ hw0/hw0.py
@@ -0,0 +1,84 @@
+# Please fill out this stencil and submit using the provided submission script.
+from GF2 import one
+from GF2 import zero
+
+
+
+
+## Problem 1
+def myFilter(L, num): return [ c for c in L if c% num != 0 ]
+
+
+
+
+## Problem 2
+def myLists(L):return [[c for c in range(1,k+1) if c ] for k in L ]
+
+
+
+## Problem 3
+def myFunctionComposition(f,g ): return {key:g[f[key]] for  key in f}
+
+
+## Problem 4
+# Please only enter your numerical solution.
+
+complex_addition_a = (3+1j) + (2 + 2j)
+complex_addition_b = (-1 + 2j) + (1 - 1j)
+
+complex_addition_c = (2 + 0j) + (-3 + .001j)
+
+complex_addition_d = 4*(0 + 2j) + (.001 + 1j)
+
+
+
+
+## Problem 5
+GF2_sum_1 = 1 
+GF2_sum_2 = 0
+GF2_sum_3 = 0
+
+
+## Problem 6
+def mySum(L):
+    current = 0
+    for x in L:
+        current = x + current
+    return current
+
+
+
+## Problem 7
+def myProduct(L):
+    current = 1
+    for x in L:
+        current = x * current
+    return current
+
+
+
+## Problem 8
+def myMin(L):
+    current = L[0]
+    for x in L:
+        if current > x:
+            current = x 
+    return current
+
+
+
+## Problem 9
+def myConcat(L):
+    current = ""
+    for x in L:
+        current = current + x 
+    return current
+
+
+## Problem 10
+def myUnion(L):
+    current = set()
+    for x in L:
+        current = current | x 
+    return current
+
